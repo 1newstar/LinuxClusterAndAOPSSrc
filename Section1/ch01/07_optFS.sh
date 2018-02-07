061c5867da99f741ce317d7a9aa109c52f42944f
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2018-02-08 07:46:20 +0800

    【ch01】 优化文件打开数

--- Section1/ch01/07_optFS.sh
+++ Section1/ch01/07_optFS.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
 ####
-# 草稿 
-####
-
-
-####
-# 优化 IP6 
-####
-# 查看 IP6 是否被启动
-lsmod | grep ipv6
-
-# 停止 IP6
-echo "install ipv6 /bin/true" 
-    > /etc/modprobe.d/disable-ipv6.conf #使用 /bin/true 代替 ipv6 模块的加载
-echo "IPV6INIT=no" 
-    >> /etc/sysconfig/network-scripts/ifcfg-eth0 #禁用基于 IP6 的网络
-
-####
 # 【ch01】 优化文件打开数 
 ####
 # 【/etc/security/limit.conf】 系统安全限制配置
