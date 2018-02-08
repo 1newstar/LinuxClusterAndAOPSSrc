a600b77c42c2b14059d930585c911bc89484cf41
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2018-02-09 06:36:22 +0800

    【ch01】 禁止远程root

--- draft.sh
+++ draft.sh
@@ -55,3 +55,15 @@ USERCTL=no #不允许普通用户修改网卡
 
 
 
+####
+# 【ch01】 禁止远程root 
+####
+sed -i 's@PermitRootLogin yes@PermitRootLogin no@' \
+    /etc/ssh/sshd_config #禁止远程 root 登录
+
+sed -i 's@PermitEmptyPasswords no@PermitEmptyPasswords no@' \
+    /etc/ssh/sshd_config #禁止空密码登录
+
+sed -i 's@UseDNS yes@UseDNS no@' \
+    /etc/ssh/sshd_config #关闭 SSH  反向查询，加快 SSH 查询速度
+
